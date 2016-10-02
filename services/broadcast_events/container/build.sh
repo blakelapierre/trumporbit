88cbdfadf3a22ff5dbba745594e9c50e3a58ef69
Blake La Pierre <blakelapierre@gmail.com>
2016-10-01 22:45:33 -0700

    containers

--- services/broadcast_events/container/build.sh
+++ services/broadcast_events/container/build.sh
@@ -3,6 +3,7 @@
 rm -rf app
 mkdir app
 
-cp -r ../.package ./app
+cp -r ../.dist ./app
+cp -r ../node_modules ./app/node_modules
 
-sudo docker build -t blakelapierre/base-node .
+sudo docker build -t trumporbit/broadcast_events .
