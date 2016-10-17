f8d4f3c0affbbffcdd0d927c1235c51f60bc4869
Blake La Pierre <blakelapierre@gmail.com>
2016-10-17 14:34:47 -0700

    Current state

--- services/broadcast_events/src/index.js
+++ services/broadcast_events/src/index.js
@@ -1,4 +1,9 @@
+// import SegfaultHandler from 'segfault-handler';
+
+// SegfaultHandler.registerHandler('crash.log');
+
 import {Server} from 'uws';
+// import {Server} from 'ws';
 
 const server = new Server({port:8080});
 
@@ -6,14 +11,84 @@ console.log('server created');
 let connected = 0;
 server.on('connection', socket => {
   const {_socket: {remoteAddress}} = socket;
-  console.log('c', ++connected, remoteAddress);
+  process.stdout.write(`c ${++connected} ${remoteAddress}\n`);
 
-  socket.on('close', () => broadcast('o-') & console.log('d', --connected, remoteAddress));
-  broadcast(`o,${remoteAddress.split('.').slice(0, 2).join('.')}`);
+  // bins['o'].push(`o,${connected},${remoteAddress.split('.').slice(0, 2).join('.')}`);
+  // socket.on('close', () => bins['o-'].push('o-') & process.stdout.write(`d ${--connected}, ${remoteAddress}\n`));
+
+
+  broadcast(`o,${connected},${remoteAddress.split('.').slice(0, 2).join('.')}`);
+  socket.on('close', () => broadcast('o-') & process.stdout.write(`d ${--connected}, ${remoteAddress}\n`));
+
+  process.nextTick(uncork);
 });
 
+let bins = { 'o': [], 'o-': [], 'lines': []},
+    oneSecondBins = { 'o': [], 'o-': [], 'lines': []},
+    targetSendsPerSecond = 60,
+    lastSend = 0,
+    lastStatus = 0;
+
+function bin(name) {
+  const value = bins[name];
+  oneSecondBins['o'].push(value);
+}
+
+function tick() {
+  const now = new Date().getTime();
+
+  if (now - lastSend > (1000 / targetSendsPerSecond)) {
+    broadcastBins(bins);
+
+    bin('o');
+    bin('o-');
+    bin('lines');
+
+    bins = { 'o': [], 'o-': [], 'lines': []};
+    lastSend = now;
+  }
+
+  if (now - lastStatus > 1000) {
+    process.stdout.write(`${new Date().getTime()} stats ${server.clients.length} ${oneSecondBins['o'].length} ${oneSecondBins['o-'].length} ${oneSecondBins['lines'].length}\n`);
+    // process.stdout.write(`status ${})
+    oneSecondBins = { 'o': [], 'o-': [], 'lines': []};
+    lastStatus = now;
+  }
+
+  // process.nextTick(tick);
+  setTimeout(tick, Math.min(0, 1000 / targetSendsPerSecond - new Date().getTime() - now));
+}
+// process.nextTick(tick);
+
+function broadcastBins(bins) {
+  broadcast(bins['o'].concat(bins['o-']).concat(bins['lines']).join('\n'));
+}
+
+function uncork() {
+  process.stdin.uncork(); // wut?
+}
+
 function broadcast(msg) {
-  server.broadcast(msg);
+  if (msg && msg.length > 0) {
+    server.broadcast(msg);
+    // console.log(`broadcasting '${msg}'`);
+  }
+}
+
+// function broadcast(msg) {
+//   server.clients.forEach(client => client.send(msg, callback));
+// }
+
+// function broadcast(msg) {
+//   server.clients.forEach(client => client.readyState === 1 && client.send(msg, callback));
+// }
+
+// function stdinCallback(error) {
+//   if (error) console.error('stdin error', error);
+// }
+
+function callback(error) {
+  if (error) console.error('Send error', error.message);
 }
 
 console.log('listeners established');
@@ -24,9 +99,12 @@ process.stdin.setEncoding('utf8');
 console.log('process listener');
 
 process.stdin.on('data', chunk => {
-  const lines = chunk.split('\n');
+  // bins['lines'].push(((chunk.toString() || '').replace(/\n\n/g, '\n').replace(/\n$/, '')));
+  broadcast((chunk || '').replace(/\n\n/g, '\n').replace(/\n$/, ''));
+  process.stdout.write(chunk);
+  // const lines = chunk.split('\n');
 
-  lines.forEach(line => line !== '' && server.broadcast(line) & console.log(line));
+  // lines.forEach(line => line !== '' && broadcast(line) & process.stdout.write(`${line}\n`));
 });
 
 console.log('listening');
